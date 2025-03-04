0b236bf456df046245796d29cd07d37193f91bca
TheLuckyman30 <bnauta0@gmail.com>
2025-03-04 14:53:04 -0500

    Users should now be able to set reminders for events by selecting them from a menu

--- src/commands/general/calendar.ts
+++ src/commands/general/calendar.ts
@@ -11,13 +11,11 @@ import {
 import { Command } from '@lib/types/Command';
 import 'dotenv/config';
 import { MongoClient } from 'mongodb';
+import { authorize } from './auth';
 //import event from '@root/src/models/calEvent';
 
-
-const fs = require('fs').promises;
 const path = require('path');
 const process = require('process');
-const { authenticate } = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 
 interface Event{
@@ -72,44 +70,6 @@ export default class extends Command {
 		const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 		const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 		const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
-		// Loads saved credentials if they exist
-		async function loadSavedCredentialsIfExist() {
-			try {
-				const content = await fs.readFile(TOKEN_PATH);
-				const credentials = JSON.parse(content);
-				return google.auth.fromJSON(credentials);
-			} catch {
-				return null;
-			}
-		}
-
-		// Saves calendar access token.json into its own folder when authenticating
-		async function saveCredentials(client) {
-			const content = await fs.readFile(CREDENTIALS_PATH);
-			const keys = JSON.parse(content);
-			const key = keys.installed || keys.web;
-			const payload = JSON.stringify({
-				type: 'authorized_user',
-				client_id: key.client_id,
-				client_secret: key.client_secret,
-				refresh_token: client.credentials.refresh_token,
-			});
-			await fs.writeFile(TOKEN_PATH, payload);
-		}
-
-		// Loads the credentials that were authenticated by the user on their first use
-		async function authorize() {
-			let client = await loadSavedCredentialsIfExist();
-			if (client) {
-				return client;
-			}
-			client = await authenticate({ scopes: SCOPES, keyfilePath: CREDENTIALS_PATH });
-			if (client.credentials) {
-				await saveCredentials(client);
-			}
-			return client;
-		}
 
 		// Formats the date and time for events
 		function formatDateTime(dateTime?: string): string {
@@ -395,7 +355,7 @@ export default class extends Command {
 
 		try {
 			await interaction.reply('Authenticating and fetching events...');
-			const auth = await authorize();
+			const auth = await authorize(TOKEN_PATH, SCOPES, CREDENTIALS_PATH);
 			await listEvents(auth, interaction, className, locationType);
 		} catch (err) {
 			console.error(err);
