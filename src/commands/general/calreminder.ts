7d948eeef5ea2a5069f95ac554a1c85527d3ee1e
TheLuckyman30 <bnauta0@gmail.com>
2025-03-03 22:45:03 -0500

    Should now be able to take in a date and time and set a reminder

--- src/commands/general/calreminder.ts
+++ src/commands/general/calreminder.ts
@@ -1,16 +1,38 @@
+import { DB } from '@root/config';
 import { Command } from '@root/src/lib/types/Command';
-import { ChatInputCommandInteraction } from 'discord.js';
+import { Reminder } from '@root/src/lib/types/Reminder';
+import { reminderTime } from '@root/src/lib/utils/generalUtils';
+import { ApplicationCommandOptionData, ApplicationCommandOptionType, ChatInputCommandInteraction } from 'discord.js';
 
 export default class extends Command {
 
 	name = 'calreminder';
-	description = 'Setup reminders from calendar events';
+	description = 'Setup reminders for calendar events';
+	options: ApplicationCommandOptionData[] =
+	[
+		{
+			name: 'time',
+			description: 'What time would you like to be reminded?',
+			type: ApplicationCommandOptionType.String,
+			required: true
+		}
+
+	];
 
 	async run(interaction: ChatInputCommandInteraction): Promise<void> {
-		await interaction.reply({
-			content: 'This is a test',
-			ephemeral: true
-		});
+		const rawDuration: string = interaction.options.getString('time');
+		const duration: Date = new Date(rawDuration);
+		const content = 'For Office Hours';
+		const repeat: 'daily' | 'weekly' = null;
+		const reminder: Reminder = {
+			owner: interaction.user.id,
+			content,
+			mode: 'public', // temporary
+			expires: duration,
+			repeat
+		};
+		interaction.client.mongo.collection(DB.REMINDERS).insertOne(reminder);
+		interaction.reply({ content: `I'll remind you about that at ${reminderTime(reminder)}.`, ephemeral: true });
 	}
 
 }
