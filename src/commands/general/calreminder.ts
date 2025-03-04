1654c323fc1a313fb0f058a7ac629d3ea42457a1
TheLuckyman30 <bnauta0@gmail.com>
2025-03-04 13:00:27 -0500

    Users can now enter a time offset for reminder

--- src/commands/general/calreminder.ts
+++ src/commands/general/calreminder.ts
@@ -3,6 +3,7 @@ import { Command } from '@root/src/lib/types/Command';
 import { Reminder } from '@root/src/lib/types/Reminder';
 import { reminderTime } from '@root/src/lib/utils/generalUtils';
 import { ApplicationCommandOptionData, ApplicationCommandOptionType, ChatInputCommandInteraction } from 'discord.js';
+import parse from 'parse-duration';
 
 export default class extends Command {
 
@@ -11,26 +12,39 @@ export default class extends Command {
 	options: ApplicationCommandOptionData[] =
 	[
 		{
-			name: 'time',
+			name: 'date',
 			description: 'What time would you like to be reminded?',
 			type: ApplicationCommandOptionType.String,
 			required: true
+		},
+		{
+			name: 'offset',
+			description: 'How long in advance',
+			type: ApplicationCommandOptionType.String,
+			required: false
 		}
 
 	];
 
 	async run(interaction: ChatInputCommandInteraction): Promise<void> {
-		const rawDuration: string = interaction.options.getString('time');
-		const duration: Date = new Date(rawDuration);
+		let offset: number;
+		const date: string = interaction.options.getString('date');
+		const rawOffset: string = interaction.options.getString('offset');
+		if (rawOffset) {
+			offset = parse(rawOffset);
+		}
+		const dateNNumber: number = new Date(date).getTime() - offset;
+		const remindDate: Date = new Date(dateNNumber);
 		const content = 'For Office Hours';
 		const repeat: 'daily' | 'weekly' = null;
 		const reminder: Reminder = {
 			owner: interaction.user.id,
 			content,
-			mode: 'public', // temporary
-			expires: duration,
+			mode: 'public',
+			expires: remindDate,
 			repeat
 		};
+
 		interaction.client.mongo.collection(DB.REMINDERS).insertOne(reminder);
 		interaction.reply({ content: `I'll remind you about that at ${reminderTime(reminder)}.`, ephemeral: true });
 	}
