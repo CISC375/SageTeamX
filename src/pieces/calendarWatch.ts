2f8efb50eeb587beb3978960d276c7c59aeda438
TheLuckyman30 <bnauta0@gmail.com>
2025-04-18 22:01:51 -0400

    Added inital webhook structure

--- src/pieces/calendarWatch.ts
+++ src/pieces/calendarWatch.ts
@@ -1,8 +1,7 @@
 import { randomUUID } from 'crypto';
 import { retrieveCalendarToken } from '../lib/auth';
-import { Client } from 'discord.js';
 
-async function register(bot: Client): Promise<void> {
+async function register(): Promise<void> {
 	const calendar = await retrieveCalendarToken();
 	console.log(await calendar.events.watch({
 		calendarId: 'c_8f94fb19936943d5980f19eac62aeb0c9379581cfbad111862852765f624bb1b@group.calendar.google.com',
