a5745c89f2bdc1fd5b03529f6ed017ce5160ca9c
TheLuckyman30 <bnauta0@gmail.com>
2025-04-22 19:40:44 -0400

    Added comments in webhook

--- src/webhooks/src/calendarWebhook.ts
+++ src/webhooks/src/calendarWebhook.ts
@@ -8,6 +8,8 @@ dotenv.config();
 
 const MONGO_URI = process.env.DB_CONN_STRING || '';
 const DB_NAME = 'CalendarDatabase';
+const CHANNEL_COLLECTION_NAME = 'watchChannels';
+const TOKEN_COLLECTION_NAME = 'syncTokens';
 
 
 const webhook = express();
@@ -45,24 +47,26 @@ async function handleChangedReminders(collection: Collection, token: string, cha
 }
 
 webhook.post('/calendarWebhook', async (req, res) => {
+	// Send a 200 OK status
 	res.sendStatus(200);
-	let collectionName = 'watchChannels';
-	console.log(req.headers);
 
+	// Connect to MongoDB and retrieve the correct watch channel
 	const client = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
 	await client.connect();
 	const db = client.db(DB_NAME);
-	let collection = db.collection(collectionName);
-	const channel: WatchChannel = await collection.findOne({ channelId: req.headers['x-goog-channel-id'] });
+	const channelCollection = db.collection(CHANNEL_COLLECTION_NAME);
+	const channel: WatchChannel = await channelCollection.findOne({ channelId: req.headers['x-goog-channel-id'] });
 
-	collectionName = 'syncTokens';
-	collection = db.collection(collectionName);
-	const syncToken: SyncToken = await collection.findOne({ calendarId: channel.calendarId });
+	// Retrive the current sync token if it exists
+	const tokenCollection = db.collection(TOKEN_COLLECTION_NAME);
+	const syncToken: SyncToken = await tokenCollection.findOne({ calendarId: channel.calendarId });
 	if (syncToken) {
-		await handleChangedReminders(collection, syncToken.token, channel);
+		// Check for changed events
+		await handleChangedReminders(tokenCollection, syncToken.token, channel);
 	} else {
+		// Insert a new sync token if one doesn't exist (Full Sync)
 		const token = await retrieveSyncToken(channel.calendarId);
-		await collection.insertOne({ token: token, calendarId: channel.calendarId });
+		await tokenCollection.insertOne({ token: token, calendarId: channel.calendarId });
 	}
 	await client.close();
 });
