57914ae7ddcaf500a9dfaf3ae192887b896c9266
TheLuckyman30 <bnauta0@gmail.com>
2025-04-22 20:24:51 -0400

    Inital implementation of chaning cal reminders

--- src/webhooks/src/calendarWebhook.ts
+++ src/webhooks/src/calendarWebhook.ts
@@ -2,8 +2,7 @@ import { retrieveEvents, retrieveSyncToken } from '../../lib/auth';
 import express from 'express';
 import { Collection, MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-import { bot } from '@root/src/sage';
-import { DB } from '@root/config';
+import { BOT, DB } from '../../../config';
 dotenv.config();
 
 const MONGO_URI = process.env.DB_CONN_STRING || '';
@@ -20,25 +19,28 @@ interface SyncToken {
 	calendarId: string;
 }
 
-
 interface WatchChannel {
 	calendarId: string,
 	channelId: string,
 	resourceId: string
 }
 
-async function handleChangedReminders(collection: Collection, token: string, channel: WatchChannel) {
+async function handleChangedReminders(collection: Collection, token: string, channel: WatchChannel, client: MongoClient): Promise<void> {
 	const changedEvents = await retrieveEvents(channel.calendarId, null, true, token);
 	const newSyncToken = await retrieveSyncToken(channel.calendarId, token);
 	await collection.updateOne({ token: token }, { $set: { token: newSyncToken } });
 
-	collection = bot.mongo.collection(DB.REMINDERS);
+	const botDB = client.db(BOT.NAME);
+	collection = botDB.collection(DB.REMINDERS);
 	const reminders = await collection.find().toArray();
 	for (const reminder of reminders) {
 		if (reminder.eventId) {
 			for (const changedEvent of changedEvents) {
 				if (changedEvent.id === reminder.eventId) {
-					//
+					const dateObj = new Date(changedEvent.start.dateTime);
+					const newExpirationDate = new Date(dateObj.getTime());
+					await collection.updateOne({ _id: reminder._id }, { $set: { expires: newExpirationDate } });
+					break;
 				}
 			}
 		}
@@ -62,7 +64,7 @@ webhook.post('/calendarWebhook', async (req, res) => {
 	const syncToken: SyncToken = await tokenCollection.findOne({ calendarId: channel.calendarId });
 	if (syncToken) {
 		// Check for changed events
-		await handleChangedReminders(tokenCollection, syncToken.token, channel);
+		await handleChangedReminders(tokenCollection, syncToken.token, channel, client);
 	} else {
 		// Insert a new sync token if one doesn't exist (Full Sync)
 		const token = await retrieveSyncToken(channel.calendarId);
