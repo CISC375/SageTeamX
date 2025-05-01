75a3bcefeeb0c6797f7f0c55d84eef81ae255e28
TheLuckyman30 <bnauta0@gmail.com>
2025-05-01 01:44:17 -0400

    Adjusted notification string format

--- src/lib/utils/webhookUtils.ts
+++ src/lib/utils/webhookUtils.ts
@@ -5,9 +5,9 @@ import { TextChannel } from 'discord.js';
 
 export async function notifyEventChange(reminder: CalReminder, newExpirationDate: Date): Promise<void> {
 	const channel = await bot.channels.fetch(CHANNELS.SAGE) as TextChannel;
-	const eventName = reminder.content.split('\n')[0];
-	const message = `Hey <@${reminder.owner}>, you had a reminder set for \`${eventName}\`.` +
-					` But that event was moved, so I updated your reminder time to ${newExpirationDate.toLocaleTimeString()}`;
+	const eventName = reminder.content.split('Starts')[0].trim();
+	const message = `Hey <@${reminder.owner}>, you had a reminder set for **${eventName}**.` +
+					` But that event was moved, so I updated your reminder time to **${newExpirationDate.toLocaleTimeString()}**.`;
 	channel.send(message);
 	return;
 }
