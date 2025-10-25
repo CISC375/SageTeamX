6037c869bbaa65efc77ce629bcd7a20820e35d44
bpollar4 <bradyp@udel.edu>
2025-10-25 14:10:11 -0400

    Streaming fixed for activity.ts overhaul for presence.js

--- src/commands/admin/activity.ts
+++ src/commands/admin/activity.ts
@@ -26,26 +26,47 @@ export default class extends Command {
 			description: 'The activity itself (ex: /help).',
 			type: ApplicationCommandOptionType.String,
 			required: true
+		},
+		{
+			name: 'url',
+			description: 'The URL for the stream (only for Streaming status).',
+			type: ApplicationCommandOptionType.String,
+			required: false
 		}
 	]
 
 	async run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
 		const bot = interaction.client;
 		const content = interaction.options.getString('content');
-		const type = interaction.options.getString('status');
-		console.log('bot has set its input to:', { type, content });
+		const type = interaction.options.getString('status') as keyof typeof ActivityType;
+		const url = interaction.options.getString('url');
 
-		const activityType = ActivityType[type as keyof typeof ActivityType];
+		if (type === 'Streaming' && !url) {
+			return interaction.reply({
+				content: 'A URL is required for the streaming status.',
+				ephemeral: true
+			});
+		}
 
-		bot.user.setActivity(content, { type: activityType });
-		
+		const activityType = ActivityType[type];
+
+		const activityOptions = {
+			type: activityType,
+			url: type === 'Streaming' ? url : undefined
+		};
+
+		bot.user.setActivity(content, activityOptions);
+
 		//	updating Sage's activity status in the database (so that it stays upon a restart)
 		bot.mongo.collection(DB.CLIENT_DATA).updateOne(
 			{ _id: bot.user.id },
-			{ $set: { status: { type, content } } },
+			{ $set: { status: { type, content, url: activityOptions.url } } },
 			{ upsert: true });
 
-		interaction.reply({ content: `Set ${BOT.NAME}'s activity to *${type} ${content}*`, ephemeral: true });
-		
-		}
+		let reply = `Set ${BOT.NAME}'s activity to *${type} ${content}*`;
+		if (type === 'Streaming') reply += ` at ${url}`;
+
+		interaction.reply({ content: reply, ephemeral: true });
 	}
+
+}
