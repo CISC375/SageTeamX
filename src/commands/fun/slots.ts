934a5159917da4bd4d99cf0d3199441c6d1f530d
bpollar4 <bradyp@udel.edu>
2025-12-01 12:57:48 -0500

    slots review

--- src/commands/fun/slots.ts
+++ src/commands/fun/slots.ts
@@ -5,7 +5,7 @@ export default class extends Command {
 
 	description = `Who doesnt love gambling, heres a slot machine.`;
 
-	//slots symbols, get more worth, 6 symbols
+	//slots symbols, get more worth, 6 symbols, 6 values, prize sort then by weight/chance of showing up
 	private symbols = [
 		{ emoji: '🍒', prize: 5, weight: 10 }, // common
 		{ emoji: '🍊', prize: 5, weight: 10 },
@@ -14,6 +14,8 @@ export default class extends Command {
 		{ emoji: '💎', prize: 50, weight: 3 },
 		{ emoji: '💰', prize: 100, weight: 1 } // rare
 	];
+
+	//earnings should be stored in mongo db that way they can be stored when the bot shuts down, and can be used in funture commands that want to use the currency
 
 	// array to generate the weighted outcomes, look to Stack overflow: https://stackoverflow.com/questions/51345563/random-numbers-with-weights-in-an-array (FOR JAVA,CONVERTED FOR TS COMMAND)
 	private weightedReel = this.symbols.flatMap(s => Array(s.weight).fill(s.emoji));
@@ -48,12 +50,12 @@ export default class extends Command {
 			const prize = Math.ceil((matchingSymbol?.prize || 0) / 4); // Win 1/4 of the prize for two
 
 			if (prize > 0) {
-				resultEmbed.addFields({ name: 'Nice!', value: `You got two matching symbols and won ${prize} coins!` });
+				resultEmbed.addFields({ name: 'wow!', value: `You got two matching symbols and won ${prize} coins!` });
 			} else {
-				resultEmbed.addFields({ name: 'So close!', value: 'Try again!' });
+				resultEmbed.addFields({ name: 'oof!', value: 'Try again!' });
 			}
 		} else {
-			resultEmbed.addFields({ name: 'Better luck next time!', value: 'No win this time.' });
+			resultEmbed.addFields({ name: 'better luck next time!', value: 'No win this time.' });
 		}
 
 		return interaction.reply({ embeds: [resultEmbed] });
